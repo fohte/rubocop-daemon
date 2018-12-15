8bbfb234d9445edf2df3c500289fa405cd3423b2
Nathan Broadbent <git@ndbroadbent.com>
2018-12-15 19:54:35 +0700

    Always pass the --color option to RuboCop::CLI, to preserve the default colorized output. (Can be turned off with --no-color.)

--- lib/rubocop/daemon/server_command/exec.rb
+++ lib/rubocop/daemon/server_command/exec.rb
@@ -5,6 +5,12 @@ module RuboCop
     module ServerCommand
       class Exec < Base
         def run
+          # RuboCop output is colorized by default where there is a TTY.
+          # We must pass the --color option to preserve this behavior.
+          unless %w[--color --no-color].any? { |f| @args.include?(f) }
+            @args.unshift('--color')
+          end
+
           RuboCop::CLI.new.run(@args)
         rescue SystemExit # rubocop:disable Lint/HandleExceptions
         end

9e22f89aaf141bed0ddfdbd8b90b94bcdddc3285
Thierry Deo <thierry.deo@gmail.com>
2022-03-25 15:52:56 +0100

    Add utils

---
+++ lib/rubocop/daemon/utils.rb
@@ -0,0 +1,20 @@
+require 'rubocop'
+require 'stringio'
+
+module RuboCop
+  module Daemon
+    module Utils
+      def self.config
+        RuboCop::ConfigStore.new.for_pwd.signature.strip
+      end
+
+      def self.versions(args)
+        cli = RuboCop::CLI.new
+        Helper.redirect(stdout: StringIO.new) do
+          cli.run([*args, '--verbose-version'])
+        end
+        RuboCop::Version.version(debug: true, env: cli.instance_variable_get(:@env)).strip
+      end
+    end
+  end
+end
